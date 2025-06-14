de57259bc49844cd958600b66a82dcdab20bf50c
carlosrandallgraida@gmail.com <carlosrandallgraida@gmail.com>
2025-06-14 09:42:20 +0800

    Added the delete functionality in the checkout section of the project

--- Amazon Project/scripts/checkout.js
+++ Amazon Project/scripts/checkout.js
@@ -1,5 +1,6 @@
-import { cart } from "../data/cart.js";
-import { products } from "../data/products.js";
+import {cart, removeFromCart} from "../data/cart.js";
+import {products} from "../data/products.js";
+import {formatCurrency} from "./utils/money.js";
 
 let cartSummaryHTML = '';
 
@@ -30,7 +31,7 @@ cart.forEach(cartItem => {
             ${matchingProduct.name}
           </div>
           <div class="product-price">
-            ${matchingProduct.priceCents / 100}
+            $${formatCurrency(matchingProduct.priceCents)}
           </div>
           <div class="product-quantity">
             <span>
@@ -39,7 +40,8 @@ cart.forEach(cartItem => {
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary jsDeleteLink"
+              data-product-Id="${matchingProduct.id}">
               Delete
             </span>
           </div>
@@ -52,7 +54,7 @@ cart.forEach(cartItem => {
           <div class="delivery-option">
             <input type="radio" checked
               class="delivery-option-input"
-              name="delivery-option-1">
+              name="delivery-option-${matchingProduct.id}">
             <div>
               <div class="delivery-option-date">
                 Tuesday, June 21
@@ -65,7 +67,7 @@ cart.forEach(cartItem => {
           <div class="delivery-option">
             <input type="radio"
               class="delivery-option-input"
-              name="delivery-option-1">
+              name="delivery-option-${matchingProduct.id}">
             <div>
               <div class="delivery-option-date">
                 Wednesday, June 15
@@ -92,6 +94,15 @@ cart.forEach(cartItem => {
       </div>
     </div>
   `;
-
-  document.querySelector('.jsOrderSummary').innerHTML = cartSummaryHTML;
 });
+document.querySelector('.jsOrderSummary')
+  .innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.jsDeleteLink')
+  .forEach(link => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
+      console.log(cart);
+    });
+  });
